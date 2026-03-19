66e3f3676a5ddded70f8c1265d91cad84f75445f
Spythere <spythere@protonmail.com>
2026-03-19 19:43:40 +0100

    chore(app): added project basic layout and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { Box } from '@mui/material';
+import { useState } from 'react';
+import type { ConversationDetails } from './types';
 
 function App() {
+  const [conversationList, setConversationList] = useState([]);
+  const [selectedConversation, setSelectedConversation] = useState<ConversationDetails | null>(null);
+
   return (
-    <Box display="flex" height="100vh" bgcolor="#121212" color="white">
-      Test
+    <Box display="flex" height="100vh">
+      {/* TODO: Sidebar component */}
+      {/* <Sidebar /> */}
+
+      <Box flex={1} p={2}>
+        {selectedConversation && (
+          <>
+            test
+            {/* <ConversationDetails  /> */}
+            {/* <NotesSection  /> */}
+          </>
+        )}
+      </Box>
     </Box>
   );
 }
---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+export type ConverstationReviewStatus = 'Pending' | 'Approved' | 'Needs Fix';
+export type ConverstationMessageSender = 'Customer' | 'AI';
+
+export interface ConversationMessage {
+  id: string;
+  sender: ConverstationMessageSender;
+  content: string;
+  timestamp: number;
+}
+
+export interface ConversationListItem {
+  id: string;
+  title: string;
+  reviewStatus: ConverstationReviewStatus;
+  lastUpdatedTimestamp: number;
+}
+
+export interface ConversationDetails extends ConversationListItem {
+  messages: any[];
+  category: string;
+}
